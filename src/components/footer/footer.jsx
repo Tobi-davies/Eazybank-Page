9f570d87113dfb8dd22f4c99cc874e9595a9909b
Adebayo <adebayotobiloba9@gmail.com>
2020-12-04 07:12:45 +0100

    updated

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,32 +1,40 @@
 import React from "react";
+import "./footer.styles.css";
 import { ReactComponent as Footerlogo } from "../../assets/imgs/logo-footer.svg";
+import CustomButton from "../custom-button/custom-button";
 
 const Footer = () => (
   <div className="footer">
-    <div className="footer-container">
-      <div className="footer-logo foot1">
-        <img src={Footerlogo} alt="logo" />
-        <div class="icon">
-          <i class="fab fa-facebook-square"></i>
-          <i class="fab fa-youtube"></i>
-          <i class="fab fa-twitter"></i>
-          <i class="fab fa-pinterest"></i>
-          <i class="fab fa-instagram"></i>
+    <div className="footer-container container">
+      <div className="first-foot">
+        <div className="footer-logo foot1">
+          <Footerlogo />
+          <div class="icon">
+            <i class="fab fa-facebook-square"></i>
+            <i class="fab fa-youtube"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-pinterest"></i>
+            <i class="fab fa-instagram"></i>
+          </div>
+        </div>
+        <div className="foot2">
+          <ul>
+            <li>About Us</li>
+            <li>Contact</li>
+            <li>Blog</li>
+          </ul>
+        </div>
+        <div className="foot3">
+          <ul>
+            <li>Careers</li>
+            <li>Support</li>
+            <li>Privacy Policy</li>
+          </ul>
         </div>
       </div>
-      <div className="foot2">
-        <ul>
-          <li>About Us</li>
-          <li>Contact</li>
-          <li>Blog</li>
-        </ul>
-      </div>
-      <div className="foot3">
-        <ul>
-          <li>Careers</li>
-          <li>Support</li>
-          <li>Privacy Policy</li>
-        </ul>
+      <div className="second-foot">
+        <CustomButton />
+        <p className="easy">© Easybank. All Rights Reserved</p>
       </div>
     </div>
   </div>
